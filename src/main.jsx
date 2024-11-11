fc9003ca7faa2ab3d9ee2ec67c5bff9c72df8222
Danila Udalchikov <githubs69@gmail.com>
2024-11-11 14:37:31 +0300

    first

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(<App />);
