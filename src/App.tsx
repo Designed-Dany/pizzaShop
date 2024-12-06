6f5c5ca2e380268c914d383c1bc0fcdfc4a4aea9
Danila Udalchikov <githubs69@gmail.com>
2024-12-06 13:51:31 +0300

    feat: reworked react.js into typescript

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Cart from "./pages/Cart";
+import FullPizza from "./pages/FullPizza";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import "./scss/app.scss";
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route path="" element={<Home />}></Route>
+					<Route path="cart" element={<Cart />}></Route>
+					<Route path="pizza/:id" element={<FullPizza />} />
+					<Route path="*" element={<NotFound />}></Route>
+				</Route>
+			</Routes>
+		</>
+	);
+}
+
+export default App;
